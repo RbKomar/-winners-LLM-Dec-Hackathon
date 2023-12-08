3e8948bcfb6093c89257e0e7a79ed64b7b524767
Laszer271 <Laszer2710@gmail.com>
2023-12-08 23:59:21 +0100

    changes in faiss

--- legacy_code_assistant/knowledge_base/knowledge_builder.py
+++ legacy_code_assistant/knowledge_base/knowledge_builder.py
@@ -1,8 +1,7 @@
 import ast
+from typing import List, Tuple
 
-# from langchain.vectorstores import FaissStore # TODO: we tu wojtek plis zamontuj tego faissa zeby nam dzialalo
-from langchain.vectorestores import FAISS
-from .embedding_processor import EmbeddingProcessor
+from langchain.vectorstores import FAISS
 from langchain.embeddings import HuggingFaceEmbeddings
 
 
@@ -44,13 +43,29 @@ class KnowledgeBaseBuilder:
                 embedding=self.processor,
             )
 
+    def search(self, query: str, k: int = 1) -> List[Tuple[str, float]]:
+        """From a query, find the elements corresponding based on personal information stored in vectordb.
+        Euclidian distance is used to find the closest vectors.
+
+        Args:
+        query (str): Question asked by the user.
+        vectordb_dir (str, optional): Path to the vectordb. Defaults to config.VECTORDB_DIR.
+
+        Returns:
+        List[Tuple[Document, float]]: Elements corresponding to the query based on semantic search, associated
+        with their respective score.
+        """
+
+        results = self.vectorstore.similarity_search_with_score(query=query, k=k)
+        return results
+
     def save_index(self):
         """Save the index."""
-        self.vectorstore.save(self.index_name)
+        self.vectorstore.save_local(self.index_name)
 
     def load_index(self):
         """Load the index."""
-        self.vectorstore = FAISS.load(self.index_name)
+        self.vectorstore = FAISS.load_local(self.index_name, embeddings =self.processor)
 
 
 
@@ -84,3 +99,18 @@ class CodeAnalyzer:
             a dictionary containing function name and function docstring.
         """
         return {'name': function_node.name, 'docstring': ast.get_docstring(function_node)}
+
+if __name__ == '__main__':
+    kbb = KnowledgeBaseBuilder()
+    kbb.upload_to_faiss({'test': 'def test():\n    pass'})
+    kbb.save_index()
+
+    kbb2 = KnowledgeBaseBuilder()
+    kbb2.load_index()
+
+    print(kbb.search('def test():\n    pass'))
+    print(kbb2.search('def test():\n    pass'))
+
+    print(kbb.search('all'))
+    print(kbb2.search('ok'))
+
