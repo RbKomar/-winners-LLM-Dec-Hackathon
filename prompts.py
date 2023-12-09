6acbd65ef323fcdd6cc881c6e9f2852139ca117a
Obrepal <i.obrepal@gmail.com>
2023-12-09 18:48:23 +0100

    added stuff for test prompt

--- prompts.py
+++ prompts.py
@@ -123,3 +123,47 @@ Q:
 Question: {question}
 Context: {context}
     """
+
+testPrompt = """Having "
+
+from django.test import TestCase
+
+from apps.corecode.models import (
+    AcademicSession,
+    AcademicTerm,
+    SiteConfig,
+    Subject,
+)
+
+
+class SiteConfigTest(TestCase):
+    def test_siteconfig(self):
+        site_config = SiteConfig.objects.create(key="akey", value="aname")
+        self.assertEqual(str(site_config), "akey")
+
+
+class AcademicSessionTest(TestCase):
+    def test_academicsession(self):
+        session = AcademicSession.objects.create(
+            name="test session", current=True)
+        self.assertEqual(str(session), "test session")
+
+
+class AcademicTermTest(TestCase):
+    def test_academicterm(self):
+        term = AcademicTerm.objects.create(name="test Term", current=True)
+        self.assertEqual(str(term), "test Term")
+
+
+class SubjectTest(TestCase):
+    def test_subject(self):
+        subject = Subject.objects.create(name="a_subject")
+        self.assertEqual(str(subject), "a_subject")"
+        
+
+as example test for django. Write me another. Remebber to take it slow and focus on step by step approach to the problem. Quality over quantity. In the end merge all  little steps.
+
+Q:
+Question: {question}
+Context: {context}
+"""
