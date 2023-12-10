01af5bf982352522ca65098574b654e3f95d56bd
Obrepal <i.obrepal@gmail.com>
2023-12-10 13:11:35 +0100

    added refactor prompt

--- demo/repo_code_graph_DEMO_2.py
+++ demo/repo_code_graph_DEMO_2.py
@@ -1,18 +1,15 @@
+from legacy_code_assistant.rag_integration.rag_manager import RagManager
+from legacy_code_assistant.knowledge_base.knowledge_graph.code_graph import CodeUsageGraphBuilder
+from streamlit_agraph import agraph, Config, Edge, Node
+import streamlit as st
+import networkx as nx
+from collections import defaultdict
 import contextlib
 import os
 import sys
 
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PROJECT_PATH)
-
-from collections import defaultdict
-
-import networkx as nx
-import streamlit as st
-from streamlit_agraph import agraph, Config, Edge, Node
-
-from legacy_code_assistant.knowledge_base.knowledge_graph.code_graph import CodeUsageGraphBuilder
-from legacy_code_assistant.rag_integration.rag_manager import RagManager
 
 
 class CodeGraphAnalyzer:
@@ -39,8 +36,10 @@ class CodeGraphAnalyzer:
             content = file.read()
         graph_builder = CodeUsageGraphBuilder(content, file_path=file_path)
         graph_builder.analyze_file()
-        self._merge_into_main_graph(graph_builder.graph, self._determine_node_size(len(content)))
-        self._update_module_node_count(file_path, len(graph_builder.graph.nodes))
+        self._merge_into_main_graph(
+            graph_builder.graph, self._determine_node_size(len(content)))
+        self._update_module_node_count(
+            file_path, len(graph_builder.graph.nodes))
 
     def _merge_into_main_graph(self, partial_graph, node_size):
         for node in partial_graph.nodes:
@@ -84,8 +83,10 @@ class CodeGraphAnalyzer:
 
     @staticmethod
     def _prepare_nodes_and_edges(graph):
-        nodes = [Node(id=n, label=n, size=data['size']) for n, data in graph.nodes(data=True)]
-        edges = [Edge(source=s, target=t, label=d.get('type', 'N/A')) for s, t, d in graph.edges(data=True)]
+        nodes = [Node(id=n, label=n, size=data['size'])
+                 for n, data in graph.nodes(data=True)]
+        edges = [Edge(source=s, target=t, label=d.get('type', 'N/A'))
+                 for s, t, d in graph.edges(data=True)]
         return nodes, edges
 
     def get_top_modules(self, k):
@@ -108,14 +109,16 @@ def get_project_path():
 def main():
     st.set_page_config(page_title="Graph Visualization", layout='wide')
     project_path = get_project_path()
-    repo_path = st.sidebar.text_input("Repository Path", os.path.join(project_path, 'tests', 'test_repo'))
+    repo_path = st.sidebar.text_input(
+        "Repository Path", os.path.join(project_path, 'tests', 'test_repo'))
 
     if 'graph_analyzer' not in st.session_state:
         st.session_state['graph_analyzer'] = CodeGraphAnalyzer(repo_path)
         st.session_state['graph_analyzer'].analyze_repository()
 
     top_modules = st.session_state['graph_analyzer'].get_top_modules(5)
-    selected_module = st.sidebar.selectbox("Select Module", [module for module, _ in top_modules])
+    selected_module = st.sidebar.selectbox(
+        "Select Module", [module for module, _ in top_modules])
 
     col1, col2 = st.columns([2, 1])
     generate_graph_button = st.sidebar.button('Generate Graph')
@@ -125,12 +128,14 @@ def main():
             st.session_state['graph_analyzer'].analyze_repository()
             st.markdown("## Code Usage Graph")
             st.session_state['graph_analyzer'].visualize_graph(selected_module)
-            st.session_state['graph'] = st.session_state['graph_analyzer'].get_graph()
+            st.session_state['graph'] = st.session_state['graph_analyzer'].get_graph(
+            )
 
     with col2:
         if 'graph' in st.session_state:
             st.markdown("## Module Details")
-            display_node_details(st.session_state['graph_analyzer'], selected_module)
+            display_node_details(
+                st.session_state['graph_analyzer'], selected_module)
 
 
 def display_node_details(graph_analyzer, module_name, top_k=10):
@@ -147,19 +152,23 @@ def display_node_details(graph_analyzer, module_name, top_k=10):
                 file_content = file.read()
 
             if file_content.strip():
-                graph_builder = CodeUsageGraphBuilder(file_content, file_path=node_data['file_path'])
+                graph_builder = CodeUsageGraphBuilder(
+                    file_content, file_path=node_data['file_path'])
                 graph_builder.analyze_file()
                 display_class_function_details(graph_builder, node_id)
-                st.markdown(f"**File Path**: [`{node_data.get('file_path', '')}`]({node_data.get('file_path', '')})")
+                st.markdown(
+                    f"**File Path**: [`{node_data.get('file_path', '')}`]({node_data.get('file_path', '')})")
                 st.markdown("---")  # Separator for each node
 
 
 def display_class_function_details(graph_builder, node_id):
     """Display details of classes and functions in the node with an 'Ask LLM' button."""
     if 'expanded_classes' not in st.session_state:
-        st.session_state['expanded_classes'] = {class_name: False for class_name in graph_builder.code_extractor.classes.keys()}
+        st.session_state['expanded_classes'] = {
+            class_name: False for class_name in graph_builder.code_extractor.classes.keys()}
     if 'expanded_functions' not in st.session_state:
-        st.session_state['expanded_functions'] = {func_name: False for func_name in graph_builder.code_extractor.functions.keys()}
+        st.session_state['expanded_functions'] = {
+            func_name: False for func_name in graph_builder.code_extractor.functions.keys()}
 
     for class_name, class_info in graph_builder.code_extractor.classes.items():
         col1, col2 = st.columns([4, 1])
@@ -167,7 +176,8 @@ def display_class_function_details(graph_builder, node_id):
             st.markdown(f"#### Class: {class_name}")
         with col1:
             expanded = st.session_state['expanded_classes'][class_name]
-            expand_button = st.button('🚀 Ask LLM', key=f'ask_llm_class_{node_id}_{class_name}')
+            expand_button = st.button(
+                '🚀 Ask LLM', key=f'ask_llm_class_{node_id}_{class_name}')
 
             if expand_button or expanded:
                 # with st.expander("LLM Prompts", expanded=expanded):
@@ -175,13 +185,15 @@ def display_class_function_details(graph_builder, node_id):
 
                 st.markdown(f"#### LLM Prompts")
                 selected_prompt = st.selectbox("Select a prompt template:",
-                                                ["Modify", "Write Tests", "Search for Vulnerabilities",
-                                                 'Explain', 'Refactor'],
-                                                key=f'prompt_select_{node_id}')
-                additional_info = st.text_area("Additional information:", key=f'additional_info_{node_id}')
+                                               ["Modify", "Write Tests", "Search for Vulnerabilities",
+                                                'Explain', 'Refactor'],
+                                               key=f'prompt_select_{node_id}')
+                additional_info = st.text_area(
+                    "Additional information:", key=f'additional_info_{node_id}')
 
                 if st.button("Submit to LLM", key=f'submit_llm_{node_id}'):
-                    process_prompt(selected_prompt, additional_info, node_id, class_info.source_code)
+                    process_prompt(selected_prompt, additional_info,
+                                   node_id, class_info.source_code)
         st.text(f"Docstring: {class_info.docstring}")
         st.markdown("```python\n" + class_info.source_code + "\n```")
 
@@ -191,7 +203,8 @@ def display_class_function_details(graph_builder, node_id):
             st.markdown(f"#### Function: {func_name}")
         with col1:
             expanded = st.session_state['expanded_functions'][func_name]
-            expand_button = st.button('🚀 Ask LLM', key=f'ask_llm_func_{node_id}_{func_name}')
+            expand_button = st.button(
+                '🚀 Ask LLM', key=f'ask_llm_func_{node_id}_{func_name}')
 
             if expand_button or expanded:
                 # with st.expander("LLM Prompts", expanded=st.session_state['expanded_functions'][func_name]):
@@ -199,13 +212,15 @@ def display_class_function_details(graph_builder, node_id):
 
                 st.markdown(f"#### LLM Prompts")
                 selected_prompt = st.selectbox("Select a prompt template:",
-                                                ["Modify", "Write Tests", "Search for Vulnerabilities",
-                                                 'Explain', 'Refactor'],
-                                                key=f'prompt_select_{node_id}')
-                additional_info = st.text_area("Additional information:", key=f'additional_info_{node_id}')
+                                               ["Modify", "Write Tests", "Search for Vulnerabilities",
+                                                'Explain', 'Refactor'],
+                                               key=f'prompt_select_{node_id}')
+                additional_info = st.text_area(
+                    "Additional information:", key=f'additional_info_{node_id}')
 
                 if st.button("Submit to LLM", key=f'submit_llm_{node_id}'):
-                    process_prompt(selected_prompt, additional_info, node_id, class_info.source_code)
+                    process_prompt(selected_prompt, additional_info,
+                                   node_id, class_info.source_code)
         st.text(f"Docstring: {func_info.docstring}")
         st.markdown("```python\n" + func_info.source_code + "\n```")
 
@@ -213,29 +228,33 @@ def display_class_function_details(graph_builder, node_id):
 def process_prompt(prompt_template, additional_info, node_id, source_code=None):
     # Placeholder function to process the prompt
     # TODO: RAG model processing logic here - bois please do this jesli możecie
-    
+
     st.write(
         f"Processing {prompt_template} with {additional_info} for node {node_id}")  # Here you would integrate your RAG model processing logic
 
-    manager = RagManager('credentials.yaml', 'docstring_based_index', 'credentials.yaml')
-    if prompt_template == 'Modify Code': # modifyPrompt - context provided
+    manager = RagManager('credentials.yaml',
+                         'docstring_based_index', 'credentials.yaml')
+    if prompt_template == 'Modify Code':  # modifyPrompt - context provided
         result = manager.modify_code(additional_info, context=source_code)
-    elif prompt_template == 'Analyze': # analyzePrompt - retrieve context
+    elif prompt_template == 'Analyze':  # analyzePrompt - retrieve context
         result = manager.analyze_code(additional_info)
-    elif prompt_template == 'Add Code': # addPrompt - retrieve context
+    elif prompt_template == 'Add Code':  # addPrompt - retrieve context
         result = manager.add_code(additional_info)
-    elif prompt_template == 'Write Tests': # testPrompt - context provided
+    elif prompt_template == 'Write Tests':  # testPrompt - context provided
         result = manager.write_tests(additional_info, context=source_code)
-    elif prompt_template == 'Search for Vulnerabilities': # vulnerabilityPrompt - context provided
-        result = manager.search_for_vulnerabilities(additional_info, context=source_code)
-    elif prompt_template == 'Ask Question': 
+    elif prompt_template == 'Search for Vulnerabilities':  # vulnerabilityPrompt - context provided
+        result = manager.search_for_vulnerabilities(
+            additional_info, context=source_code)
+    elif prompt_template == 'Ask Question':
         raise NotImplementedError
     elif prompt_template == 'Refactor':
-        raise NotImplementedError
+        result = manager.search_for_vulnerabilities(
+            additional_info, context=source_code)
     else:
         raise ValueError(f"Invalid prompt template: {prompt_template}")
 
     st.markdown(result)
+
 
 if __name__ == '__main__':
     main()
--- legacy_code_assistant/rag_integration/rag_manager.py
+++ legacy_code_assistant/rag_integration/rag_manager.py
@@ -9,7 +9,7 @@ from legacy_code_assistant.knowledge_base.knowledge_builder import KnowledgeBase
 from langchain.embeddings import AzureOpenAIEmbeddings
 
 from legacy_code_assistant.rag_integration.rag_prompts import (
-    modifyPrompt, analyzePrompt, addPrompt, testPrompt, vulnerabilityPrompt)
+    modifyPrompt, analyzePrompt, addPrompt, testPrompt, vulnerabilityPrompt, refactorPrompt)
 
 from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
@@ -39,7 +39,6 @@ class RagManager:
         os.environ["AZURE_OPENAI_ENDPOINT"] = credentials['AZURE_OPENAI_ENDPOINT']
         os.environ["AZURE_OPENAI_API_KEY"] = credentials['AZURE_OPENAI_API_KEY']
 
-
         self.model = AzureChatOpenAI(
             openai_api_version="2023-05-15",
             azure_deployment=credentials['Deployment_completion'],
@@ -50,11 +49,10 @@ class RagManager:
         )
         self.df = pd.read_csv(filepath)
 
-
-        self.kbb_docs = KnowledgeBaseBuilder(index_name=index_name, model=self.embeddings)
+        self.kbb_docs = KnowledgeBaseBuilder(
+            index_name=index_name, model=self.embeddings)
         self.kbb_docs.load_index()
         self.retriever = self.kbb_docs.get_retriever()
-
 
     def _build_chain(self, template, context=None):
         prompt = ChatPromptTemplate.from_template(template)
@@ -69,14 +67,14 @@ class RagManager:
             )
         else:
             chain = (
-                {'context': context, 
+                {'context': context,
                  "question": RunnablePassthrough()}
                 | prompt
                 | self.model
                 | StrOutputParser()
             )
         return chain
-    
+
     def _run_chain(self, prompt_template, user_input, context=None):
         chain = self._build_chain(prompt_template, context=context)
         result = chain.invoke(user_input)
@@ -96,7 +94,6 @@ class RagManager:
 
     def search_for_vulnerabilities(self, user_input, context=None):
         return self._run_chain(vulnerabilityPrompt, user_input, context=context)
-    
-    def refactor_code(self, user_input):
-        raise NotImplementedError
-    
+
+    def refactor_code(self, user_input, context=None):
+        return self._run_chain(refactorPrompt, user_input, context=context)
